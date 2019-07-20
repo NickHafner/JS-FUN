5fe868b534386543e0bc026225bf9d2e21ee09c0
Nicholas Hafner <nickhafner@outlook.com>
2019-07-20 13:33:29 -0500

    Starting Conway's game of life

---
+++ src/components/GameOfLife.vue
@@ -0,0 +1,25 @@
+<template>
+    <canvas id="game" :width="canvasWidth" :height="canvasHeight"></canvas>
+</template>
+<script>
+export default {
+    name: 'GameOfLife',
+    data() {
+        return {
+            
+        }
+    },
+    beforeMount() {
+
+    },
+    methods: {
+        onStart () {
+
+        }
+    }
+}
+
+</script>
+<style scoped>
+</style>
+
